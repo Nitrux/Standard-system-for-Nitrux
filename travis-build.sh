fb2bce9a1ca9ed177bcd91684fc840a7ccdcb557
Uri Herrera <hello@uriherrera.com>
2020-08-25 04:24:17 -0500

    add conflicts

--- travis-build.sh
+++ travis-build.sh
@@ -11,9 +11,10 @@ git_commit=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: nitrux-standard" \
-	"Version: 0.1.15-$git_commit" \
+	"Version: 0.2.0-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
+	"Conflicts: nitrux-standard-legacy" \
 	"Architecture: amd64" \
 	"Description: The Nitrux standard system."
 
